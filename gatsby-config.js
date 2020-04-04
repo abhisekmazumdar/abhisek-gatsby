2a063fb034bcb6c2ee68413ba850e57a6c9dd049
Abhisek Mazumdar <abhisekmajumdar4@gmail.com>
2020-04-04 21:26:59 +0530

    gulp and sass setup with some footer work.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Abhisek Mazumdar`,
+    description: `Just a simple site with gatsby as front end and drupal 8 backend.`,
+    author: `@justAbhisek`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-drupal`,
       options: {
@@ -31,11 +32,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/my-pic.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
