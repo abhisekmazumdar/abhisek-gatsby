1157d3b5695147fbd4009bbfd909a1131f1f4960
Abhisek Mazumdar <abhisekmajumdar4@gmail.com>
2020-04-16 02:22:38 +0530

    Footer re-done

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,12 @@ const Footer = () => (
   <footer>
     <div className='wrapper'>
       <div className='content'>
-          © Abhisek Mazumdar,
-          <span className='footerYear'>{new Date().getFullYear()}</span>
+        <nav className='menu'>
+          {/* Don't add more then 4 Links */}
+          <a href="https://twitter.com/abhisekmajumdar" target="_blank" rel="noopener noreferrer">Twitter</a>
+          <a href="https://www.instagram.com/abhisekmazumdar/" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a href="https://www.drupal.org/u/abhisekmazumdar" target="_blank" rel="noopener noreferrer">Drupal</a>
+        </nav>
       </div>
     </div>
   </footer>
