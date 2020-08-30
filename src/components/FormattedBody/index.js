f5830aa077b3c54e5121d37bd6f3164ec699cece
Abhisek Mazumdar <abhisekmajumdar4@gmail.com>
2020-08-30 12:09:33 +0530

    Blog view page added image option on the sub sections.

--- src/components/FormattedBody/index.js
+++ src/components/FormattedBody/index.js
@@ -1,12 +1,32 @@
 import React from "react"
+import Img from "gatsby-image"
+
+import "../../sass/_formattedbody.sass"
 
 const FormattedBody = data => (
-  <div className="block">
-    <span className="lable">{data.block.field_title}</span>
-    <div
-      className="section-content"
+  <div className="block-wrapper">
+    {data.block.field_title && (
+      <span className="lable">{data.block.field_title}</span>
+    )}
+    {data.block.relationships && (
+      <div
+        className={
+          "block-media " + data.block.field_media_position.toLowerCase()
+        }
+      >
+        <Img
+          fluid={
+            data.block.relationships.field_sub_media.relationships
+              .field_media_image.localFile.childImageSharp.fluid
+          }
+        ></Img>
+      </div>
+    )}
+    <span
       dangerouslySetInnerHTML={{ __html: data.block.field_body.processed }}
-    ></div>
+      className="block-content"
+    ></span>
+
     {console.log(data.block)}
   </div>
 )
